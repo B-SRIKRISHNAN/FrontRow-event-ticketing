ba61329049baccc4e73961852d90eb932039cd51
srikrishnan <bsk19.05.1998@gmail.com>
2026-09-26 01:02:13 +0530

    chore: add jsconfig and next.config with root path alias configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,13 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        '@': './',
+      },
+    },
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
